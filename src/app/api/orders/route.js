5740d9836f814c24091c578f16b6dc6cf66b7025
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-10-04 16:44:45 +0330

    adding GET fuction in order route

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -3,10 +3,10 @@ import path from "path";
 
 export  async function POST(req) {
   try {
-    const { name, product, price } = await req.json();
+    const { name, product, price,cost } = await req.json();
     const filePath = path.join(process.cwd(), "data", "orders.txt");
 
-    const ordersData = `Name : ${name}, product : ${product}, price : ${price}\n`;
+    const ordersData = `Name : ${name}, product : ${product}, price : ${price} , cost : ${cost}\n`;
 
     const dirPath = path.dirname(filePath);
     await fs.mkdir(dirPath, { recursive: true });
@@ -26,3 +26,24 @@ export  async function POST(req) {
 }
 
 
+
+export async function GET(req) {
+  try {
+    const filePath = path.join(process.cwd(),'data','orders.txt')
+    await fs.access(filePath)
+    const data = await fs.readFile(filePath, 'utf-8');
+    const products = data.split('\n').filter(line => line).map(line => {
+      const [product, materialsPart,pricePart,costPart] = line?.split(', ');
+    return {
+      product: product.split(': ')[1],
+      materials: materialsPart.split(': ')[1],
+      price: pricePart.split(': ')[1],
+      cost: costPart.split(': ')[1],
+    }
+    })
+    return new Response(JSON.stringify(products), { status: 200 });
+  } catch (err) {
+    console.error('Error reading file:', err);
+    return new Response(JSON.stringify({ message: 'Error reading products' }), { status: 500 });
+  }
+}
